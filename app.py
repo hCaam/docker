6fbfea13bec9e93f7ede28ed13a8a4258b6e39be
Taj Hakam Ikhsan <94432199+hCaam@users.noreply.github.com>
2024-04-15 08:10:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Python goes brrrrrrrrrrr!"
+    return "swarm goes brrrrrrrrrrr!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
